ac4433d0fbe23e55450799fc6b40b3f328f9c47e
Mike12138210 <3192742021@qq.com>
2026-07-13 15:14:25 +0800

    fix: 增加按键消抖延时，解决按键按下无反应的问题

--- 3-1 独立按键控制LED亮灭/main.c
+++ 3-1 独立按键控制LED亮灭/main.c
@@ -13,22 +13,21 @@ void Delay(unsigned int t){ while(t--); } // 简单延时
 
 void main(){
 	while(1){
-		/* if(TXD == 0){          // 检测到按下
-            Delay(1000);       // 延时约10ms,跳过抖动
-            if(TXD == 0){      // 再次确认确实按下了
-                LED1 = 0;
-                LED2 = 0;
-            }
-        }else{
-            // 松开后熄灭
-            LED1 = 1;
-            LED2 = 1;
-            Delay(1000);
-        } */
-		if(TXD == 0){ // sbit TXD = P3^1;
-			LED1 = 0; // 点亮
-		}else{
-		  LED1 = 1; // 熄灭
-		}
+		if(TXD == 0){          // 检测到按下
+      Delay(1000);       // 延时约10ms,跳过抖动
+      if(TXD == 0){      // 再次确认确实按下了
+        LED1 = 0;
+        LED2 = 0;
+      }else{
+        // 松开后熄灭
+        LED1 = 1;
+        LED2 = 1;
+        Delay(1000);
+      } 
+//		if(TXD == 0){ // sbit TXD = P3^1;
+//			LED1 = 0; // 点亮
+//		}else{
+//		  LED1 = 1; // 熄灭
+//		}
 	}
 }
