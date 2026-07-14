b195719406cc0f6c1b2c6aca21cacd364fdd5d24
Mike12138210 <3192742021@qq.com>
2026-07-14 15:45:23 +0800

    feat: 增加首次按键标志，实现上电全灭、首次按键点亮第一个灯

--- 3-4 独立按键控制LED移位/main.c
+++ 3-4 独立按键控制LED移位/main.c
@@ -1,16 +1,11 @@
 #include <reg52.h>
 #include <intrins.h>
 
-sbit LED1 = P2^0;  // 将P2.0命名为LED1
-sbit LED2 = P2^1;
-sbit LED3 = P2^2;
-sbit LED4 = P2^3;
-sbit LED5 = P2^4;
-sbit LED6 = P2^5;
-sbit LED7 = P2^6;
+sbit LED1 = P2^0;  // 将P2.0命名为LED1（最左边）
 sbit LED8 = P2^7;
 
 unsigned char LEDNum; // 全局变量默认为0
+bit isFirst = 1; // 首次标志，1表示尚未显示初始灯
 
 void Delay(unsigned int xms)		//@11.0592MHz
 {
@@ -29,28 +24,42 @@ void Delay(unsigned int xms)		//@11.0592MHz
 
 void main(){
 	while(1){
+		// 右移
 		if(TXD == 0){ // sbit TXD   = P3^1;
 			Delay(20);
 			while(TXD == 0);
 			Delay(20);
 
-			if(LEDNum == 8){
-				LEDNum = 0;
+			if(isFirst){
+				P2 = ~(0x01 << LEDNum); // LEDNum=0，显示0xFE
+				isFirst = 0;
+			}else{
+				if(LEDNum == 7){
+					LEDNum = 0;
+				}else{
+					LEDNum++;
+				}
+			P2 = ~(0x01 << LEDNum);
 			}
-			P2 = ~(0x01<<LEDNum);
-			LEDNum++;
 		}
+		
+		// 左移
 		if(RXD == 0){ // sbit RXD   = P3^0;
 			Delay(20);
 			while(RXD == 0);
 			Delay(20);
-
-			if(LEDNum == 0){
-				LEDNum = 7;
-			}else{
-				LEDNum--;
-			}
-			P2 = ~(0x01<<LEDNum);
+		
+				if(isFirst){
+					P2 = ~(0x01 << LEDNum); // LEDNum = 0，显示0xFE
+					isFirst = 0;
+				}else{
+					if(LEDNum == 0){
+						LEDNum = 7;
+					}else{
+						LEDNum--;
+					}
+					P2 = ~(0x01 << LEDNum);
+				}
 		}
 	}
 }
