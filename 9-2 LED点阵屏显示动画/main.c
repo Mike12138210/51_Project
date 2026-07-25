ec727d1ac09e01f8260672c3c013350ebbf5f660
Mike12138210 <3192742021@qq.com>
2026-07-25 16:07:22 +0800

    feat: LED点阵屏升级为多帧动画播放，并封装硬件初始化
    - main.c: 将静态单帧图像升级为3帧动画（心跳效果），帧切换间隔100ms
    - main.c: 增加currentFrame与frameDelay变量，实现帧序列循环
    - MatrixLED.c: 新增MatrixLED_Init()函数，封装SCK/RCK引脚初始化
    - 中断服务: 分离列扫描(1ms)与帧切换(100ms)逻辑，保证显示与动画互不干扰

---
+++ 9-2 LED点阵屏显示动画/main.c
@@ -0,0 +1,45 @@
+#include <reg52.h>
+#include "Timer0.h"
+#include "MatrixLED.h"
+
+
+
+unsigned char currentFrame;
+unsigned char frameCounter; // 用于控制帧切换速度（帧延时计数器）
+
+// 存储图像数据（每一列对应一个字节）
+unsigned char code LEDAnimation[][8] = {
+    {0x30, 0x48, 0x44, 0x22, 0x22, 0x44, 0x48, 0x30},
+		{0x7E, 0x81, 0x81, 0x81, 0x81, 0x81, 0x81, 0x7E},
+		{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+};
+
+void main(){
+	MatrixLED_Init();
+	Timer0_Init();
+	while(1){
+
+	}
+}
+
+void Timer0_Routine() interrupt 1{
+	static unsigned char currentColumn;
+	static unsigned int frameDelay; // 帧切换计时器
+  TL0 = 0x66; // 设置定时初值
+  TH0 = 0xFC; // 设置定时初值
+	
+	MATRIX_LED_PORT = 0xFF;
+	MatrixLED_ShowColumn(currentColumn,LEDAnimation[currentFrame][currentColumn]);
+	
+	currentColumn++;
+	if(currentColumn >= 8) currentColumn = 0;
+	
+	frameDelay++;
+	if(frameDelay >= 100){
+		frameDelay = 0;
+		currentFrame++;
+		if(currentFrame >= 3){
+			currentFrame = 0;
+		}
+	}
+}
